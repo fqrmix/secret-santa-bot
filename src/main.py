9720456105bfbd3e2cabfd1a97908de15e154f80
fqrmix <semen.cavallini@gmail.com>
2022-12-17 16:29:37 +0300

    Vov@n added

--- src/main.py
+++ src/main.py
@@ -13,8 +13,6 @@ async def main():
 
     dp.include_router(pollanswer.router)
 
-    # Запускаем бота и пропускаем все накопленные входящие
-    # Да, этот метод можно вызвать даже если у вас поллинг
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
