ad540cf01dc30266ee721a4942038dedb1ce9543
fqrmix <semen.cavallini@gmail.com>
2022-12-17 16:07:17 +0300

    Some decor added

--- src/data/users.py
+++ src/data/users.py
@@ -7,12 +7,19 @@ class Answers:
             self.json_input = json.load(file)
             print(self.json_input)
 
-    def get_info(self, id=None):
-        if id is not None:
-            return self.json_input[id]['reply']
+    def get_info(self):
         return self.json_input
+
+    def get_reply(self, id: int):
+        return self.json_input[id]['reply']
 
     def set_reply(self, id: str, value: bool):
         with open(self.file_path, "w+", encoding='utf-8-sig') as file:
             self.json_input[id]['reply'] = value
             json.dump(self.json_input, file, ensure_ascii=False, indent=4)
+
+    def all_replied(self) -> bool:
+        for item in self.json_input.values():
+            if item['reply'] == '❌ Не отправил':
+                return False
+        return True
--- src/handlers/pollanswer.py
+++ src/handlers/pollanswer.py
@@ -9,23 +9,41 @@ answers = Answers()
 
 @router.message(commands="santa")
 async def message_with_text(message: Message):
-    info_list = answers.get_info()
-    message_text = ''
-    for data in info_list:
-        message_text += f"{info_list[data]['name']} - {info_list[data]['reply']}\n"
-    await message.answer(message_text, reply_markup=get_inline_kb())
+    if answers.all_replied():
+        await message.answer('🎁 Похоже, что все отправили свои пожелания! Пора запускать шарманку.')
+    else:
+        info_list = answers.get_info()
+        message_text = ''
+        for data in info_list:
+            message_text += f"{info_list[data]['reply']} - *{info_list[data]['name']}*\n"
+        await message.answer(
+            message_text, 
+            reply_markup=get_inline_kb(), 
+            parse_mode='markdown'
+        )
 
 @router.callback_query(text="set_answer")
 async def send_random_value(callback: CallbackQuery):
-    answers.set_reply(str(callback.from_user.id), 'Отправил')
-    info_list = answers.get_info()
-    message_text = ''
-    reply_count = 0
-    for data in info_list:
-        message_text += f"{info_list[data]['name']} - {info_list[data]['reply']}\n"
-        if info_list[data]['reply'] == 'Отправил':
-            reply_count += 1
-    if reply_count == 6:
-        await callback.message.answer('Похоже, что все отправили свои пожелания! Пора запускать шарманку.')
+    if answers.get_reply(str(callback.from_user.id)) == '🎁 Отправил':
+        await callback.answer(
+            text="Ты уже отправил пожелания!",
+            show_alert=True
+        )
     else:
-        await callback.message.answer(message_text, reply_markup=get_inline_kb())
+        answers.set_reply(str(callback.from_user.id), '🎁 Отправил')
+        info_list = answers.get_info()
+        message_text = f"🎁 {info_list[str(callback.from_user.id)]['name']} отправил пожелания!\n\n"
+        for data in info_list:
+            message_text += f"{info_list[data]['reply']} - *{info_list[data]['name']}*\n"
+        await callback.answer(
+            text="Ты успешно отправил пожелания!",
+            show_alert=True
+        )
+        if answers.all_replied():
+            await callback.message.answer('🎁 Похоже, что все отправили свои пожелания! Пора запускать шарманку.')
+        else:
+            await callback.message.answer(
+                message_text, 
+                reply_markup=get_inline_kb(), 
+                parse_mode='markdown'
+            )
